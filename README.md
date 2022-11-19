f522431ec54a4304b198f188b35fdf753de67b8a
joonto <joonto7828@gmail.com>
2022-11-19 20:01:49 +0900

    coin top 100 data

---
+++ README.md
@@ -0,0 +1,25 @@
+# Python CoinMarketCap marketcap top 100 listing data
+
+top 100 ( sorted by marketcap ) coinmarketcap coinlist data.
+
+
+---
+
+## installation
+__ First step __
+- pip install -r requirements.txt
+
+__ Second step __
+- you should get a apikey from coinmarketcap link [apiLink](https://coinmarketcap.com/api/)
+
+__ Third step __
+- if you have environment variable named "apiKey" in your environment file or in your local system, then just just activate python file
+
+    > python getTop100CoinData.py
+- or if you don't have environment variable named "apiKey", you should give apikey in the prompt parameter when you activate python file  
+
+    > [example this is fake apikey] python getTop100CoinData.py 62fsh9tv-slkf9-3k56-8de8-sdfjblk9ca2
+
+__ Fifth step __
+
+- you can get a list data that is same with coinmarketcap main page. you can slice each data with comma ',' . 
---
+++ getTop100CoinData.py
@@ -0,0 +1,35 @@
+from coinmarketcapapi import CoinMarketCapAPI
+import os
+from dotenv import load_dotenv
+import logging
+import sys
+
+load_dotenv()
+
+api_key = os.environ.get('apiKey')
+cmc = CoinMarketCapAPI()
+
+STOP_ON_ERROR = True
+DEBUG = True
+
+
+if api_key is not None:
+    api_data = api_key
+else:
+    api_data = sys.argv[1]
+
+cmc = CoinMarketCapAPI(api_key=api_data, debug=True, logger=logging.getLogger(__name__)) # Sandbox
+
+fullData = cmc.cryptocurrency_listings_latest()
+data_list = []
+range_int = 0
+
+for eachData in fullData.data:  
+    
+    coinData = str(eachData['name']) + ',' + str(eachData['quote']['USD']['price']) + ',' + str(eachData['quote']['USD']['price'])  + ',' +  str(eachData['quote']['USD']['percent_change_1h'])  + ',' +  str(eachData['quote']['USD']['volume_change_24h']) + ',' +  str(eachData['quote']['USD']['percent_change_7d'])  + ',' + str(eachData['quote']['USD']['market_cap']) + ',' +  str(eachData['quote']['USD']['volume_24h']) + ',' +  str(eachData['total_supply'])
+    data_list.append(coinData)
+
+    range_int+=1
+    if range_int == 100:
+        print(data_list)
+        break    
